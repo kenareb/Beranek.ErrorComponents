c6d55f0eb0f87fb7a9d6238fce91016ae957bbf7
Marcus Beranek <marcus.beranek@live.de>
2020-04-14 22:13:59 +0200

    Adjusted renaming in supression file.

--- Beranek.ErrorComponents/GlobalSuppressions.cs
+++ Beranek.ErrorComponents/GlobalSuppressions.cs
@@ -4,4 +4,5 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1816:Dispose-Methoden müssen SuppressFinalize aufrufen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Beranek.ErrorComponents.DelayedAction.Dispose")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2008:Keine Tasks ohne Übergabe eines TaskSchedulers erstellen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Beranek.ErrorComponents.DelayedTask.RunWithDelayAsync(System.Int32)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2008:Keine Tasks ohne Übergabe eines TaskSchedulers erstellen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Beranek.ErrorComponents.DelayedAction.RunWithDelayAsync(System.Int32)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1816:Dispose-Methoden müssen SuppressFinalize aufrufen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Beranek.ErrorComponents.TimedAction.Dispose")]
